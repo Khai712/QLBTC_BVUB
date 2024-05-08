6145704dc09ec68ac5769a5cd6ba171c0ffc3c47
Admin <demonsofthekid@gmail.com>
2024-05-08 08:38:34 +0700

    Khởi tạo spring boot

---
+++ src/main/java/QLBTC_BVUB/QlbtcBvubApplication.java
@@ -0,0 +1,13 @@
+package QLBTC_BVUB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlbtcBvubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QlbtcBvubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/QLBTC_BVUB/QlbtcBvubApplicationTests.java
@@ -0,0 +1,13 @@
+package QLBTC_BVUB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QlbtcBvubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
